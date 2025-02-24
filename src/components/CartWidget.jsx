f66c72dcd2836a2517d034b98bce10eff1c75264
Victor Cubas <vhcubas91@gmail.com>
2025-02-24 13:36:20 -0300

    [fix] se ajusta el estilo css del cart

--- src/components/CartWidget.jsx
+++ src/components/CartWidget.jsx
@@ -2,10 +2,14 @@ import { MdOutlineShoppingCart } from "react-icons/md"
 
 const CartWidget = () => {
     return(
-        <div className="flex items-center">
-            <MdOutlineShoppingCart />
-            <span>5</span>
-        </div>
+        <div className="relative inline-flex">
+            <div className="rounded-md bg-slate-800 py-1 px-2 border border-transparent text-center text-xl text-white transition-all shadow-md hover:shadow-lg focus:bg-slate-700 focus:shadow-none active:bg-slate-700 hover:bg-slate-700 active:shadow-none disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none" type="button">
+                <MdOutlineShoppingCart />
+            </div>
+            <span className="absolute top-1 right-0 grid min-h-[24px] min-w-[24px] translate-x-2/4 -translate-y-2/4 place-items-center rounded-full bg-red-600 py-1 px-1 text-xs text-white">
+            5
+            </span>
+      </div>
     );
 }
 
