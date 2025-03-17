eb2686a894edf8311bbf7c8c1a10ba6865a2a728
Victor Cubas <vhcubas91@gmail.com>
2025-03-17 07:27:54 -0300

    [feat] se agrega react-router-dom las configuraciones del routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,29 @@
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import './App.css'
 import ItemListContainer from './components/ItemListContainer'
 import NavBar from './components/NavBar'
+import ItemDetailContainer from './components/ItemDetailContainer'
+import { ErrorPage } from './components/ErrorPage'
 
 function App() {
 
   return (
     <>
-      <NavBar/>
-      <ItemListContainer greeting='Bienvenido'/>
+      <BrowserRouter>
+        <NavBar/>
+
+        <main className="mt-6 pt-5">
+          <Routes>
+            <Route path='/' element={<ItemListContainer greeting='Bienvenido'/>}/>
+            <Route path='/category/:categoryId' element={<ItemListContainer greeting='Bienvenido A categoria'/>}/>
+            <Route path='/category/:categoryId/:eventId' element={<ItemDetailContainer />}/>
+            <Route path='*' element={<ErrorPage/>}/>
+          </Routes>
+        </main>
+
+
+        {/* footer */}
+      </BrowserRouter>
     </>
   )
 }
