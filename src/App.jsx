906b5a7cfcd22ab0f6fba08f106d8f21354e1f33
Victor Cubas <vhcubas91@gmail.com>
2025-03-29 08:11:50 -0300

    [feat] se agrega config basica de firebase

--- src/App.jsx
+++ src/App.jsx
@@ -4,24 +4,32 @@ import ItemListContainer from './components/ItemListContainer'
 import NavBar from './components/NavBar'
 import ItemDetailContainer from './components/ItemDetailContainer'
 import { ErrorPage } from './components/ErrorPage'
+import { CartContextProvider } from './context/CartContext'
+import CartContainer from './components/CartContainer'
+import { useEffect } from 'react'
+import { getItems } from './firebase'
 
 function App() {
+  useEffect(() => {
+    getItems();
+  }, []);
 
   return (
     <>
       <BrowserRouter>
-        <NavBar/>
+        <CartContextProvider>
+          <NavBar/>
 
-        <main className="mt-6 pt-5">
-          <Routes>
-            <Route path='/' element={<ItemListContainer greeting='Bienvenido'/>}/>
-            <Route path='/category/:categoryId' element={<ItemListContainer greeting='Bienvenido A categoria'/>}/>
-            <Route path='/category/:categoryId/:eventId' element={<ItemDetailContainer />}/>
-            <Route path='*' element={<ErrorPage/>}/>
-          </Routes>
-        </main>
-
-
+          <main className="mt-6 pt-5">
+            <Routes>
+              <Route path='/' element={<ItemListContainer greeting='Bienvenido'/>}/>
+              <Route path='/category/:categoryId' element={<ItemListContainer greeting='Bienvenido A categoria'/>}/>
+              <Route path='/category/:categoryId/:eventId' element={<ItemDetailContainer />}/>
+              <Route path='/cart' element={<CartContainer />}/>
+              <Route path='*' element={<ErrorPage/>}/>
+            </Routes>
+          </main>
+        </CartContextProvider>
         {/* footer */}
       </BrowserRouter>
     </>
---
+++ src/firebase.js
@@ -0,0 +1,25 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+import { collection, getDocs, getFirestore } from "firebase/firestore";
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyANnoPz_8h4UwLpFpsO9vH9jzNPedDamWs",
+  authDomain: "coder-ecommerce-5a3ce.firebaseapp.com",
+  projectId: "coder-ecommerce-5a3ce",
+  storageBucket: "coder-ecommerce-5a3ce.firebasestorage.app",
+  messagingSenderId: "468126883578",
+  appId: "1:468126883578:web:1dde5e0237eaf6526d7343"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+const db = getFirestore(app);
+
+export const getItems = async () => {
+    const querySnapshot = await getDocs(collection(db, 'items'));
+    querySnapshot.forEach(doc => console.log(`${doc.id} -> ${JSON.stringify(doc.data())}`));
+}
