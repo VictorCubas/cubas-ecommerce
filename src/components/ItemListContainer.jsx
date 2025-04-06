08bd3200d2ff2a302cc26ba84885f15d3d7d6027
Victor Cubas <vhcubas91@gmail.com>
2025-04-06 20:46:10 -0300

    [feat] se agrega un toast cuando el carrito esta vacio

--- src/components/ItemListContainer.jsx
+++ src/components/ItemListContainer.jsx
@@ -3,10 +3,26 @@ import EventoList from "./EventoList";
 import CustomSpinner from "./CustomSpinner.jsx";
 import { useParams } from "react-router-dom";
 import { useFetch } from "../hooks/useFetch.js";
-
+import { toast, Toaster } from 'react-hot-toast';
+import { useCart } from "../hooks/useCart.js";
+import { useEffect } from "react";
 
 const ItemListContainer = ({greeting, user}) => {
     const {categoryId} = useParams();
+    const {accedienteACheckoutVacio, toggleCheckoutVacio} = useCart();
+
+
+    //control en caso de que se haya accedido a checkout y cart este vacio
+    useEffect(() => {
+        if(accedienteACheckoutVacio){
+            const duration = 3500;
+            toast.error("Tu carrito aún está vacío", {duration: duration})
+
+            setTimeout(() => {
+                toggleCheckoutVacio();
+            }, duration);
+        }
+    }, []);
 
     const {loading, 
         fetchedData: data, 
@@ -22,6 +38,11 @@ const ItemListContainer = ({greeting, user}) => {
 
             {loading && <CustomSpinner/>}
             {!loading && <EventoList eventos={data}/>}
+
+            {accedienteACheckoutVacio && <Toaster
+                position="bottom-center"
+                reverseOrder={false}
+                />}
         </>
     )
 }
