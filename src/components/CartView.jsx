0fe029376b29c1a6225518ff247c67533fcfb427
Victor Cubas <vhcubas91@gmail.com>
2025-04-10 19:36:44 -0300

    [fix] se maneja el modal para limpiar el carrito

--- src/components/CartView.jsx
+++ src/components/CartView.jsx
@@ -1,65 +1,91 @@
-import React from 'react';
-import { Card, CardBody, Typography, Button } from "@material-tailwind/react";
+import React, { useState } from 'react';
+import { 
+  Card, 
+  CardBody, 
+  Typography, 
+  Button, 
+  Dialog,
+  DialogHeader,
+  DialogBody,
+  DialogFooter
+} from "@material-tailwind/react";
+
+
+
 import CartItem from './CartItem';
 import { useCart } from '../hooks/useCart';
 import { Link } from 'react-router-dom';
-
-
+import ModalDeleteConfirmation from './ModalDeleteConfirmation';
 
 const CartView = () => {
-    const {cart, clearCart, cartTotal} = useCart();
+  const {cart, cartTotal} = useCart();
+  const [showModalConfirmation, setShowModalConfirmation] = useState(false);
+  
+  /**
+   * Controla que el modal de confirmacion se muestre o no
+   * @param {*} show 
+   */
+  const handleShowModal = (show) => {
+    setShowModalConfirmation(show);
+  }
 
-  return (
-    <div className='flex justify-center'>
-      <Card className="w-5/6">
-      <CardBody>
-        <div className="mb-4 flex items-center justify-between">
-          <Typography variant="h5" color="blue-gray" className="">
-            Tu carrito
-          </Typography>
-          <Typography
-            variant="small"
-            color="blue"
-            className="font-bold"
-            role='button'
-            onClick={clearCart}
-          >
-            Limpiar carrito
-          </Typography>
+  return ( 
+    <>
+      {!showModalConfirmation ? 
+        <div className='flex justify-center'>
+          <Card className="w-5/6">
+          <CardBody>
+            <div className="mb-4 flex items-center justify-between">
+              <Typography variant="h5" color="blue-gray" className="">
+                Tu carrito
+              </Typography>
+              <Typography
+                variant="small"
+                color="blue"
+                className="font-bold"
+                role='button'
+                onClick={() => handleShowModal(true)}
+              >
+                Limpiar carrito
+              </Typography>
 
-        </div>
-        <div className="divide-y divide-gray-200 ">
-          {cart.map((evento) => (
+            </div>
+            <div className="divide-y divide-gray-200 ">
+              {cart.map((evento) => (
 
-            <CartItem key={evento.id} evento={evento}/>
-          ))}
-        </div>
+                <CartItem key={evento.id} evento={evento}/>
+              ))}
+            </div>
 
-          <div className='pt-5 mt-5 flex justify-end w-ful'>
-            <Typography color="blue-gray" variant="h4">
-                  Total: ${cartTotal()},00
-            </Typography>
-          </div>
+              <div className='pt-5 mt-5 flex justify-end w-ful'>
+                <Typography color="blue-gray" variant="h4">
+                      Total: ${cartTotal()},00
+                </Typography>
+              </div>
 
 
-          <div className='flex gap-3 mt-3'>
-            <Link to='/'>
-              <Button color='amber' size="lg"
-                  variant="outlined" className='text-gray-800 hover:text-amber-500'>
-                      Seguir comprando 
-              </Button>
-            </Link>
+              <div className='flex gap-3 mt-3'>
+                <Link to='/'>
+                  <Button color='amber' size="lg"
+                      variant="outlined" className='text-gray-800 hover:text-amber-500'>
+                          Seguir comprando 
+                  </Button>
+                </Link>
 
-            <Link to='/checkout'>
-              <Button color='amber' size="lg"
-                  variant="filled" className='text-white hover:bg-amber-300 text-md font-normal hover:text-orange-800 hover:shadow-none'>
-                      Terminar Compra
-              </Button>
-            </Link>
-          </div>
-      </CardBody>
-      </Card>
-    </div>
+                <Link to='/checkout'>
+                  <Button color='amber' size="lg"
+                      variant="filled" className='text-white hover:bg-amber-300 text-md font-normal hover:text-orange-800 hover:shadow-none'>
+                          Terminar Compra
+                  </Button>
+                </Link>
+              </div>
+          </CardBody>
+          </Card>
+        </div> : 
+      <>   
+          <ModalDeleteConfirmation handleShowModal={handleShowModal}/>
+      </>}
+    </>
   )
 }
 
