fb767adb552b96a5ccd1d69bee31f0125817dcb2
Victor Cubas <vhcubas91@gmail.com>
2025-04-13 10:57:22 -0300

    [fix] se corrige el toast duplicado

--- src/components/ToastAddedCartSuccess.jsx
+++ src/components/ToastAddedCartSuccess.jsx
@@ -9,38 +9,43 @@ const ToastAddedCartSuccess = () => {
 
     //control en caso de que se haya accedido a checkout y cart este vacio
     useEffect(() => {
-        if(showToastAddedCartSuccess){
-            const {eventName, quantity} = showToastAddedCartSuccess;
-            const duration = 3500;
-            hasShown.current = true;
-
-            toast.custom((t) => (
-                <div
-                  className={`${
-                    t.visible ? 'animate-enter' : 'animate-leave'
-                  } max-w-md w-full bg-white shadow-lg rounded-lg pointer-events-auto flex ring-1 ring-black ring-opacity-5`}
-                >
-                  <div className="flex-1 w-0 p-4">
-                    <div className="flex items-start">
-                      <div className="h-12 w-10 flex items-center justify-center text-2xl">
-                        <FaCircleCheck className='text-green-500 font-semibold text-2xl'/>
-                      </div>
-                      <div className="ml-3">
-                        {`Se agregó ${quantity} ticket${quantity > 1 ? 's': ''} para el evento `}
-                        <span className='text-gray-900 font-semibold'>{eventName}</span>
-                         {` al carrito`}
-                      </div>
+        if (showToastAddedCartSuccess) {
+          const { eventName, quantity } = showToastAddedCartSuccess;
+          const duration = 3500;
+          const toastId = 'added-to-cart-toast';
+      
+          //elimina el toast anterior si es que existiera
+          toast.dismiss(toastId);
+      
+          toast.custom(
+            (t) => (
+              <div
+                className={`${
+                  t.visible ? 'animate-enter' : 'animate-leave'
+                } max-w-md w-full bg-white shadow-lg rounded-lg pointer-events-auto flex ring-1 ring-black ring-opacity-5`}
+              >
+                <div className="flex-1 w-0 p-4">
+                  <div className="flex items-start">
+                    <div className="h-12 w-10 flex items-center justify-center text-2xl">
+                      <FaCircleCheck className="text-green-500 font-semibold text-2xl" />
+                    </div>
+                    <div className="ml-3">
+                      {`Se agregó ${quantity} ticket${quantity > 1 ? 's' : ''} para el evento `}
+                      <span className="text-gray-900 font-semibold">{eventName}</span>
+                      {` al carrito`}
                     </div>
                   </div>
                 </div>
-              ))
-
-            setTimeout(() => {
-                turnOffToastAddedCartSuccess();
-                hasShown.current = false; 
-            }, duration);
+              </div>
+            ),
+            { id: toastId, duration }
+          );
+      
+          setTimeout(() => {
+            turnOffToastAddedCartSuccess();
+          }, duration);
         }
-    }, [showToastAddedCartSuccess]);
+      }, [showToastAddedCartSuccess]);
 
 
   return (
